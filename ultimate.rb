3566de09ddde86568cba9657c23cc80746c96397
n-mouse <murahoid@gmail.com>
2015-01-19 23:35:48 +0200

    new season, faction_ids

--- ultimate.rb
+++ ultimate.rb
@@ -4,50 +4,100 @@ require 'mechanize'
 require 'csv'
 require 'json'
 
-def validate_presence (date)
-  if date.nil?
-    puts "При запуску треба ввести дві дати у форматі dd.mm.yyyy"
-    abort
+class Validator
+
+  def validate_presence (date)
+    if date.nil?
+      puts "При запуску треба ввести дві дати у форматі dd.mm.yyyy"
+      abort
+    end
+  end
+
+  def validate_format(date)
+    begin
+      Date.strptime(date,"%d.%m.%Y")
+    rescue ArgumentError
+      puts "Неправильний формат дати"
+      abort
+    end
+  end
+
+  def validate_sequence(a,b)
+    if a > b
+      puts "Дати не в тому порядку"
+      abort
+   # elsif a < Date.strptime("12.12.2012","%d.%m.%Y")
+    #  puts "Дані раніше 12.12.2012 не збираються"
+     # abort
+    #elsif b > Date.strptime("31.12.2014","%d.%m.%Y")
+     # puts "За 2015 і пізніше даних ще нема, ви чо"
+      #abort
+    end
   end
 end
 
 
-def validate_format(date)
-  begin
-    Date.strptime(date,"%d.%m.%Y")
-  rescue ArgumentError
-    puts "Неправильний формат дати"
-    abort
-  end
-end
 
-def validate_sequence(a,b)
-  if a > b
-    puts "Дати не в тому порядку"
-    abort
-  elsif a < Date.strptime("12.12.2012","%d.%m.%Y")
-    puts "Дані раніше 12.12.2012 не збираються"
-    abort
-  elsif b > Date.strptime("31.12.2014","%d.%m.%Y")
-    puts "За 2015 і пізніше даних ще нема, ви чо"
-    abort
+class Voting
+  
+  attr_reader :page
+  
+  def initialize(page)
+    @page = page
+  end
+ 
+  def element 
+    page.search('tr')[1].children.children
+  end
+ 
+  def day
+    element[5].text.gsub(/\n/, "").match(/від .{10}/).to_s.gsub(/від/,'')
+  end
+  
+  def time
+    element[5].text.gsub(/\n/, "")[-8,8]
+  end
+  
+  def thisid
+    utime=day+" "+time
+    DateTime.strptime(utime, " %d.%m.%Y %H:%M:%S").to_time.to_i
+  end
+  
+  def title
+    element[8].text.gsub(/\n/, "")
+  end
+  
+  def gangs
+    array_of_rows = page.search('tr')
+    empty = array_of_rows.find_all { |r| r.text == " "}
+    delimiters = empty.map { |e| array_of_rows.index(e)+1 }
+    delimiters = delimiters.unshift(2)
+    n = delimiters.size - 2
+    gangs = Array.new
+    a = *(0..n)
+    for i in a
+      gangs << array_of_rows.slice(delimiters[i]..delimiters[i+1])
+    end
+    gangs
   end
   
 end
 
+GANGS = {"ПОРОШЕНКА" => 1, "ФРОНТ" => 2, "Опозиційний"=> 3, "Позафракційні" => 4, "САМОПОМІЧ" => 5, 
+"Ляшка"=> 6, "Воля" => 7, "Батьківщина" => 8, "розвиток" => 9}
 
 date1=ARGV[0]
 date2=ARGV[1]
 
-validate_presence(date1)
-validate_presence(date2)
+validator = Validator.new
 
-a = validate_format(date1)
-b = validate_format(date2)
+validator.validate_presence(date1)
+validator.validate_presence(date2)
 
-validate_sequence(a,b)
+a = validator.validate_format(date1)
+b = validator.validate_format(date2)
 
-agent = Mechanize.new
+validator.validate_sequence(a,b)
 
 dates_file=File.read("dates.json")
 dates_hash = JSON.parse(dates_file)
@@ -79,6 +129,7 @@ CSV.open("#{date1}_#{date2}_MPs.csv", "ab") do |data|
   header[0] = "voting_ID"
   header[1] = "MP_ID"
   header[2] = "voting"
+  header[3] = "faction_id"
   data << header
 end
 
@@ -102,87 +153,69 @@ CSV.open("#{date1}_#{date2}_frakciji.csv", "ab") do |data|
   header[6] = "absent"
   data << header
 end
-      
-      
+
+agent = Mechanize.new
+
 kods.each do |id|
   begin
+ 
   page = agent.get("http://w1.c1.rada.gov.ua/pls/radan_gs09/ns_golos_print?g_id="+id.to_s+"&vid=1")
-
-  n = page.search('tr').length-1  
   
-  element = page.search('tr')[1].children.children
-  day = element[5].text.gsub(/\n/, "").match(/від .{10}/).to_s.gsub(/від/,'')
-  time = element[5].text.gsub(/\n/, "")[-8,8]
-      
-  utime=day+" "+time
-  thisid = DateTime.strptime(utime, " %d.%m.%Y %H:%M:%S").to_time.to_i
-  fraks[0] = thisid
+  voting = Voting.new(page)  
+ 
+  thisid = voting.thisid
+  
   noms[0] = thisid
-  noms[1] = day
-  noms[2] = time
-  noms[3] = element[8].text.gsub(/\n/, "")
+  noms[1] = voting.day
+  noms[2] = voting.time
+  noms[3] = voting.title
+  
   CSV.open("#{date1}_#{date2}_ids.csv", "ab", {:col_sep => "\t"}) do |data|
     data << noms
   end
-    
-  arr = *(2..n)
-
-  for i in arr
-    if page.search('tr')[i].children.size > 2
-      arow = page.search('tr')[i].text.gsub(/\n/,',').split(',')
-      
-      bag_of_dicks.each do |dick|
-        if dick.include?(arow[1])
-         nards[0] = thisid
-          nards[1] = dick[1]
-          nards[2] = arow[2]
-          CSV.open("#{date1}_#{date2}_MPs.csv", "ab") do |data|
-           data << nards
-          end
-        elsif dick.include?(arow[3])
-          nards[0] = thisid
-          nards[1] = dick[1]
-          nards[2] = arow[4]
-          CSV.open("#{date1}_#{date2}_MPs.csv", "ab") do |data|
-           data << nards
+     
+  voting.gangs.each do |gang|
+    GANGS.keys.each do |key|
+      if gang.first.text.match(key)
+        fra_id = GANGS[key]
+        fraks[0] = thisid
+        fraks[1] = fra_id
+        fraks[2] = gang[0].text.match(/За - \d*/).to_s.gsub(/\n/, "").gsub(/За\s-\s/,"")
+        fraks[3] = gang[0].text.match(/Проти - \d*/).to_s.gsub(/\n/, "").gsub(/Проти\s-\s/,"")
+        fraks[4] = gang[0].text.match(/Утрималися - \d*/).to_s.gsub(/\n/, "").gsub(/Утрималися\s-\s/,"")
+        fraks[5] = gang[0].text.match(/Не голосували - \d*/).to_s.gsub(/\n/, "").gsub(/Не\sголосували\s-\s/,"")
+        fraks[6] = gang[0].text.match(/Відсутні - \d*/).to_s.gsub(/\n/, "").gsub(/Відсутні\s-\s/,"")
+        CSV.open("#{date1}_#{date2}_frakciji.csv", "ab") do |data|
+          data << fraks
+        end
+        gang.each do |row|
+          arow = row.text.gsub(/\n/,',').split(',')
+          bag_of_dicks.each do |dick|
+            if dick.include?(arow[1])
+              nards[0] = thisid
+              nards[1] = dick[1]
+              puts nards[1].to_s + " dep"
+              nards[2] = arow[2]
+              nards[3] = fra_id
+              CSV.open("#{date1}_#{date2}_MPs.csv", "ab") do |data|
+                data << nards
+              end
+            elsif dick.include?(arow[3])
+              nards[0] = thisid
+              nards[1] = dick[1]
+              nards[2] = arow[4]
+              nards[3] = fra_id
+              CSV.open("#{date1}_#{date2}_MPs.csv", "ab") do |data|
+                data << nards
+              end
+            end
           end
         end
       end
-    elsif page.search('tr')[i].children.size == 2 && page.search('tr')[i].text.size > 2
-      case page.search('tr')[i].text
-      when /Фракція.*регіонів/
-        fraks[1] = 3
-      when /Група.*європейська/
-        fraks[1] = 8
-      when /Група.*розвиток/
-        fraks[1] = 7
-      when /Фракція.*Комуністичної/
-        fraks[1] = 4
-	  when /Фракція.*Батьківщина/
-        fraks[1] = 1
-	  when /Фракція.*Кличка/
-        fraks[1] = 2
-      when  /Фракція.*Свобода/
-        fraks[1] = 6
-      when /стабільність/
-        fraks[1] = 9
-      when /Позафракційні/
-        fraks[1] = 5
-      end
-      element2 = page.search('tr')[i].text.split(/\n/)[2]
-      fraks[2] = element2.match(/За - \d*/).to_s.gsub(/\n/, "").gsub(/За\s-\s/,"")
-      fraks[3] = element2.match(/Проти - \d*/).to_s.gsub(/\n/, "").gsub(/Проти\s-\s/,"")
-      fraks[4] = element2.match(/Утрималися - \d*/).to_s.gsub(/\n/, "").gsub(/Утрималися\s-\s/,"")
-      fraks[5] = element2.match(/Не голосували - \d*/).to_s.gsub(/\n/, "").gsub(/Не\sголосували\s-\s/,"")
-      fraks[6] = element2.match(/Відсутні - \d*/).to_s.gsub(/\n/, "").gsub(/Відсутні\s-\s/,"")
-      CSV.open("#{date1}_#{date2}_frakciji.csv", "ab") do |data|
-        data << fraks
-      end
-      
     end
   end
-  sleep 1
-  puts id
+  puts id  
+  sleep 2
   rescue
     next
   end
